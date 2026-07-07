539e7a940b27a99d0b07f852ab5111393d2bc836
Badalbhai Ineshbhai Vasava <badalvasava286@gmail.com>
2026-07-07 13:15:00 +0530

    Fixed missing save and added component layout

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
   return (
     <div className="bg-black min-h-screen font-sans selection:bg-blue-500 selection:text-white scroll-smooth">
       <main>
-        <AboutTabs />
         <Hero />
-        <SqlTerminal />
         <DashboardSandbox />
+        <Experience />
+        <AboutTabs />
+        <SqlTerminal />
         <Projects />
         <Contact />
-        <Experience />
       </main>
 
       <AiChatWidget />
